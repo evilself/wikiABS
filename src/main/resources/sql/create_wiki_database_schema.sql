a889162e6f198262ff9dd1a52ba511130f614e09
evilself <borismechkov@gmail.com>
2014-11-30 00:01:44 -0600

    added lines to the schema - SecurityInfo table

--- src/main/resources/sql/create_wiki_database_schema.sql
+++ src/main/resources/sql/create_wiki_database_schema.sql
@@ -11,6 +11,15 @@ CREATE TABLE User_Roles
 	PRIMARY KEY (ROLE_ID)
 );	
 
+CREATE TABLE SECURITY_INFO
+(
+	SECURITY_INFO_ID int not null AUTO_INCREMENT,
+	SECURITY_QUESTION varchar(128) not null,
+    SECURITY_ANSWER VARCHAR(64) NOT NULL,
+    USERNAME varchar(32) not null,
+	PRIMARY KEY (SECURITY_INFO_ID)
+);	
+
 CREATE TABLE User
 (
 	USER_ID int not null AUTO_INCREMENT,
@@ -75,7 +84,7 @@ insert into product (PRODUCT_DESCRIPTION, PRODUCT_IDENTITY, PRODUCT_NAME) values
 
 insert into user_roles (ROLE, USERNAME) values ("ADMIN", "admin");
 insert into user (FIRST_NAME, LAST_NAME, PASSWORD, USERNAME, USER_ROLE_ID) values ("Admin", "Adminov", '$2a$10$HCjaHWSbeObVjSWdJkq2n.udUSxSYnuN2FpBvQ9iOYxbTtgGpeMaa', "admin", 1);
-
+insert into security_info (SECURITY_QUESTION, SECURITY_ANSWER, USERNAME) values ("What is the best car?", "Porsche", "admin");
 drop database wikiABS;
 
 select * from article;
@@ -83,6 +92,7 @@ select * from product;
 select * from user;
 select * from user_roles;
 select * from attachment;
+select * from security_info;
 
 delete from user_roles where role_id = 2;
 delete from article where article_id = 9;
